de7f55ca1fada0fec798c05e22a1ba83ea1e6973
dearlsh94 <dearlsh94@gmail.com>
2025-09-07 14:48:20 +0900

    content: 소개 문구를 변경한다.

--- blog.config.ts
+++ blog.config.ts
@@ -7,7 +7,7 @@ export default {
   seo: {
     title: 'treefeely',
     name: 'treefeely',
-    description: 'Articles are contributed, mostly about the web, but also the author\'s own thoughts; writing is not their profession, but they retire to write fragrant pieces.',
+    description: 'I write to survive within the crisis of narrative.',
   },
   articles: {
     contentDirectory: 'content',
@@ -29,7 +29,8 @@ export default {
     repo: 'dearlsh94/blog-comments',
   },
   heroText: `
-  더 따뜻하고 편리한 세상이 되기를 바라며 개발자로 일하고 있는 이승환(Ethan)입니다. 종종 글을 씁니다.
+  더 따뜻하고 편리한 세상이 되기를 바라며 개발자로 일하고 있습니다.\n
+  서사의 위기 속에 살아남기 위해 글을 씁니다.
   `,
   contacts: {
     github: 'https://github.com/dearlsh94',
