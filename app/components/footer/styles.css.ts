7400501d40f598c0858d4d5abacc8e12ce0bb1dd
dearlsh94 <dearlsh94@gmail.com>
2025-09-07 16:30:44 +0900

    style: Footer의 borderTop을 제거한다.

--- app/components/footer/styles.css.ts
+++ app/components/footer/styles.css.ts
@@ -3,7 +3,6 @@ import { style } from '@vanilla-extract/css';
 export const footer = style({
   marginTop: '2rem',
   padding: '2rem 0 1rem 0',
-  borderTop: '1px solid var(--color-border, #e0e0e0)',
   display: 'flex',
   justifyContent: 'flex-start',
   alignItems: 'center',
