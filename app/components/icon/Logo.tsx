9423e5b58737ddc7028ee2b906fab7041ec6d440
dearlsh94 <dearlsh94@gmail.com>
2025-10-08 00:12:45 +0900

    feat: Logo에 invert 인터페이스를 추가한다.

--- app/components/icon/Logo.tsx
+++ app/components/icon/Logo.tsx
@@ -2,10 +2,13 @@ import LogoImage from './logo.png';
 
 interface LogoProps {
   size: number;
+  invert?: boolean;
 }
 
-export function Logo({ size }: LogoProps) {
-  return (
-    <img src={LogoImage} alt="logo" width={size} />
-  );
+export function Logo({ size, invert = false }: LogoProps) {
+  const style = {
+    filter: invert ? 'invert(1)' : 'invert(0)',
+  };
+
+  return <img src={LogoImage} alt="logo" width={size} style={style} />;
 }
